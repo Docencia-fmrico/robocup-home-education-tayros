052a08578a938fbbd686649606d72f15bf86eb2d
madport <portanova2002@gmail.com>
2022-04-28 11:43:23 +0200

    Movement
    Signed-off-by: madport <portanova2002@gmail.com>

--- tayjarvis/src/follow_BT/Follow.cpp
+++ tayjarvis/src/follow_BT/Follow.cpp
@@ -1,48 +1,50 @@
 #include <string>
-#include "follow_BT/target_reached.h"
+#include "follow_BT/Follow.h"
 #include "behaviortree_cpp_v3/behavior_tree.h"
-#include "std_msgs/Int32.h"
 #include "ros/ros.h"
 #include <unistd.h>
+#include "std_msgs/Int32.h"
 
 enum
 {
     STOP = 0,
-    TF = 1,
+    GO = 1,
 };
 
 namespace follow_BT
 {
 
-target_reached::target_reached(const std::string& name,  const BT::NodeConfiguration & config)
+Follow::Follow(const std::string& name,  const BT::NodeConfiguration & config)
 : BT::ActionNodeBase(name, config), nh_("~")
 {
   std::string activation_pub =  nh_.param("set_goal_activation_topic", std::string("/tay_ros/set_goal_activation"));
+  
   act_pub_ = nh_.advertise<std_msgs::Int32>(activation_pub, 1);
 }
 
 void
-target_reached::halt()
+Follow::halt()
 {
-  //ROS_INFO("Target reached halt");
+ // ROS_INFO("SetGoal halt");
 }
 
 BT::NodeStatus
-target_reached::tick()
+Follow::tick()
 {
   std_msgs::Int32 state;
-  state.data = STOP;
+  state.data = GO;
   act_pub_.publish(state);
 
+  setOutput<move_base_msgs::MoveBaseGoal>("pos", goal_);
+  return BT::NodeStatus::SUCCESS;
+}
 
-  ROS_INFO("Where I am?????????????????");
-  return BT::NodeStatus::FAILURE;
-}  
 
-}// namespace follow_BT
+} // namespace follow_BT
+
 
 #include "behaviortree_cpp_v3/bt_factory.h"
 BT_REGISTER_NODES(factory)
 {
-  factory.registerNodeType<follow_BT::target_reached>("Target_reached");
+  factory.registerNodeType<follow_BT::Follow>("Follow");
 }
